be5840c623c0e5316889bc4cbe2db1ab034ecac5
dimaliwatkent <dimaliwatkent@gmail.com>
2024-01-04 20:33:21 +0800

    updated navbar and events buttons

--- app/components/Events.jsx
+++ app/components/Events.jsx
@@ -1,24 +1,49 @@
+"use client";
+import { useState } from "react";
 import { Heading, Button, Card, Flex, Box, Text } from "@radix-ui/themes";
 import React from "react";
 import EventCard from "./EventCard";
 
 const Events = () => {
+  const [activeButton, setActiveButton] = useState("upcoming"); // Default active button
+
+  const handleButtonClick = (event) => {
+    setActiveButton(event.target.innerText.toLowerCase());
+  };
+  console.log(activeButton);
+
   return (
     <div className=" my-8">
       <Heading color="green" size="8" align="center" my="5">
         EVENTS
       </Heading>
       <Flex my="5" gap="3" justify="center">
-        <Button variant="soft" color="amber">
-          Upcomming
+        <Button
+          variant="soft"
+          color={activeButton === "upcoming" ? "amber" : "gray"}
+          onClick={handleButtonClick}
+        >
+          Upcoming
         </Button>
-        <Button variant="soft" color="gray">
+        <Button
+          variant="soft"
+          color={activeButton === "ongoing" ? "amber" : "gray"}
+          onClick={handleButtonClick}
+        >
           Ongoing
         </Button>
-        <Button variant="soft" color="gray">
+        <Button
+          variant="soft"
+          color={activeButton === "done" ? "amber" : "gray"}
+          onClick={handleButtonClick}
+        >
           Done
         </Button>
-        <Button variant="soft" color="gray">
+        <Button
+          variant="soft"
+          color={activeButton === "cancelled" ? "amber" : "gray"}
+          onClick={handleButtonClick}
+        >
           Cancelled
         </Button>
       </Flex>
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { Button, Flex, Text, Container } from "@radix-ui/themes";
 import Image from "next/image";
 import React from "react";
@@ -5,9 +7,11 @@ import Link from "next/link";
 import CicsLogo from "../assets/cics_logo.png";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <>
-      <div className="w-full h-20 absolute z-50">
+      <div className="w-full h-20 fixed z-50 backdrop-blur-sm bg-black/50">
         <div className="container mx-auto px-4 h-full">
           <Container size="3">
             <div className="flex justify-between items-center h-full gap-6 ">
@@ -16,22 +20,38 @@ const Navbar = () => {
               </Text>
               <Image src={CicsLogo} alt="CICS Logo" width={70} height={70} />
               <ul className="hidden md:flex gap-x-10 text-white ">
-                <li className="hover:text-yellow-500">
+                <li
+                  className={`font-bold ${
+                    pathname === "/" ? "text-yellow-500" : ""
+                  }`}
+                >
+                  <Link href="/">
+                    <p>Home</p>
+                  </Link>
+                </li>
+                <li
+                  className={`${
+                    pathname === "/events" ? "text-yellow-500" : ""
+                  }`}
+                >
                   <Link href="/events">
                     <p>Events</p>
                   </Link>
                 </li>
-                <li className="hover:text-yellow-500">
-                  <Link href="/about">
-                    <p>About Us</p>
-                  </Link>
-                </li>
-                <li className="hover:text-yellow-500">
+                <li
+                  className={`${
+                    pathname === "/services" ? "text-yellow-500" : ""
+                  }`}
+                >
                   <Link href="/services">
                     <p>Services</p>
                   </Link>
                 </li>
-                <li className="hover:text-yellow-500 ">
+                <li
+                  className={`${
+                    pathname === "/contacts" ? "text-yellow-500" : ""
+                  }`}
+                >
                   <Link href="/contacts">
                     <p>Contacts</p>
                   </Link>
